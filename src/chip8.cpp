62eaf06086a98f06a220ca2f6672677e378c2c0d
Davoud Ghafouri <ghafooridavood580@gmail.com>
2026-06-17 14:54:30 +0330

    still adding new instructions support

--- src/chip8.cpp
+++ src/chip8.cpp
@@ -220,10 +220,110 @@ void Chip8::OP_Bnnn() {
   pc = address + registers[0];
 }
 
-
 // load Vx  random bytes & kk
 void Chip8::OP_Cxkk() {
-    uint8_t X = (opcode &0x0F00) >> 8u;
-    uint8_t kk =  (opcode & 0x00FF);
-    registers[X] = randByte(randGen) & kk;
+  uint8_t X = (opcode & 0x0F00) >> 8u;
+  uint8_t kk = (opcode & 0x00FF);
+  registers[X] = randByte(randGen) & kk;
 }
+
+//  Display n-byte sprite starting at memory location I at (Vx, Vy)
+//  set VF = collision.
+void Chip8::OP_Dxyn() {
+  uint8_t X = (opcode & 0x0F00) >> 8u;
+  uint8_t Y = (opcode & 0x00F0) >> 4u;
+  uint8_t byte = (opcode & 0x000F);
+
+  // we wrap this positions up so if Vx was bigger than WEISTH_PIXLE_SIZE
+  // or HEIGHT_PIXLE_SIZE we could able to draw sprite
+  uint8_t xs = registers[X] % WEIDTH_PIXEL_SIZE;
+  uint8_t ys = registers[Y] % HEIGHT_PIXEL_SIZE;
+
+  // if collision accurd this flage sets to 1
+  registers[0xF] = 0;
+
+  // TODO :IMPLEMENT THIS FLAGE LATER
+}
+
+// skipped next intruction if key with value of Vx is pressed
+void Chip8::OP_Ex9E() {
+  uint8_t X = (opcode & 0x0F00) >> 8u;
+  uint8_t pressedkey = registers[X];
+
+  if (keypad[pressedkey]) {
+    pc += 2;
+  }
+}
+
+// skipped next intruction if key with value of Vx is not pressed
+void Chip8::OP_ExA1() {
+  uint8_t X = (opcode & 0x0F00) >> 8u;
+  uint8_t pressedkey = registers[X];
+
+  if (!keypad[pressedkey]) {
+    pc += 2;
+  }
+}
+
+// Set Vx = delay timer value.
+void Chip8::OP_Fx07() {
+  uint8_t X = (opcode & 0x0F00) >> 8u;
+  registers[X] = dt;
+}
+
+// which key is pressed set it to X register
+void Chip8::OP_Fx0A() {
+  uint8_t X = (opcode & 0x0F00) >> 8u;
+  if (keypad[0]) {
+    registers[X] = 0;
+  } else if (keypad[1]) {
+    registers[X] = 1;
+  } else if (keypad[2]) {
+    registers[X] = 2;
+  } else if (keypad[3]) {
+    registers[X] = 3;
+  } else if (keypad[4]) {
+    registers[X] = 4;
+  } else if (keypad[5]) {
+    registers[X] = 5;
+  } else if (keypad[6]) {
+    registers[X] = 6;
+  } else if (keypad[7]) {
+    registers[X] = 7;
+  } else if (keypad[8]) {
+    registers[X] = 8;
+  } else if (keypad[9]) {
+    registers[X] = 9;
+  } else if (keypad[10]) {
+    registers[X] = 10;
+  } else if (keypad[11]) {
+    registers[X] = 11;
+  } else if (keypad[12]) {
+    registers[X] = 12;
+  } else if (keypad[13]) {
+    registers[X] = 13;
+  } else if (keypad[14]) {
+    registers[X] = 14;
+  } else if (keypad[15]) {
+    registers[X] = 15;
+  }
+}
+
+// set dellay timer to X
+void Chip8::OP_Fx15(){
+    uint8_t X = (opcode & 0x0F00) >> 8u;
+    dt = registers[X];
+}
+
+// set dellay timer to X
+void Chip8::OP_Fx18(){
+    uint8_t X = (opcode & 0x0F00) >> 8u;
+    st = registers[X];
+}
+
+// add VX to I
+void Chip8::OP_Fx1E(){
+    uint8_t X = (opcode & 0x0F00) >> 8u;
+    index += registers[X];
+}
+
